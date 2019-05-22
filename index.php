4ef01925629292c6d3aad41e41ead4e5a7fd4421
Sawanon <sawanon01@hotmail.com>
2019-05-22 10:31:51 +0700

    submit #2

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    06:30 นม ขนมปัง 10+15 = <?php echo $pay[]=25; ?> บาท <br>
+    06:51 MRT = <?php echo$pay[]=21; ?> บาท <br>
+    07:11 รถเมล์ 205 = <?php echo$pay[]=8; ?> บาท <br>
+    12:00 กินข้าว = <?php echo$pay[]=40; ?> บาท <br>
+    17:00 รถเมล์ 205 = <?php echo$pay[]=8; ?> บาท <br>
+    18:00 MRT = <?php echo$pay[]=21; ?> บาท <br>
+    19:00 กินข้าว <?php echo$pay[]=40; ?> บาท <br>asd
+    <?php
+    foreach ($pay as $key => $value) {
+      echo $value += $value;
+    }
+     ?>
+  </body>
+</html>
